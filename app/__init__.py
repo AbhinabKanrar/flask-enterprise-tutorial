874ce90bed86a0a7b7e8da13cf7605d8f0770933
RSAdmin <abhinabkanrar@gmail.com>
2018-06-21 18:33:59 +0530

    logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,36 @@
 from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
+from logging.config import dictConfig
 
 from app.common.domain.schema.error import Error
 from app.common.util.resp_mapper import create_resp
 
+import logging
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 
 app.config.from_object('config.settings')
 app.config.from_envvar('FLASK_ENV',silent=True)
+
+logger = logging.getLogger('FlaskLogger')
+logger.addHandler(logging.handlers.TimedRotatingFileHandler('flask.log', when="d", interval=1, backupCount=5))
 
 db = SQLAlchemy(app)
 
